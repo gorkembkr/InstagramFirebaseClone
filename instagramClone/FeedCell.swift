b4bb5e71bb8881d3f7f3d8625e4b983cb1b71995
gorkembkr <gorkembekar@gmail.com>
2021-08-31 14:16:20 +0300

    first commit

---
+++ instagramClone/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  instagramClone
+//
+//  Created by Görkem on 12.07.2021.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
